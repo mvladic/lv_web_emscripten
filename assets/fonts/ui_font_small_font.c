1d79d0024d5fe96322a320889b9bc4efcc5ac2fe
Martin Vladic <martin.vladic@gmail.com>
2022-11-11 14:30:11 +0100

    update eez-framework

--- assets/fonts/ui_font_small_font.c
+++ assets/fonts/ui_font_small_font.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Size: 16 px
  * Bpp: 8
- * Opts: --user-data-dir=C:\Users\mvladic\AppData\Roaming\eezstudio --app-path=C:\work\eez\studio --no-sandbox --no-zygote --node-integration-in-worker --lang=en-US --device-scale-factor=1 --num-raster-threads=4 --enable-main-frame-before-activation --renderer-client-id=5 --launch-time-ticks=676605954103 --mojo-platform-channel-handle=2620 --field-trial-handle=1716,i,17393657524418861288,11050729133581670415,131072 --disable-features=SpareRendererForSitePerProcess,WinRetrieveSuggestionsOnlyOnDemand /prefetch:1
+ * Opts: --user-data-dir=C:\Users\mvladic\AppData\Roaming\eezstudio --app-path=C:\work\eez\studio --no-sandbox --no-zygote --node-integration-in-worker --lang=en-US --device-scale-factor=1 --num-raster-threads=4 --enable-main-frame-before-activation --renderer-client-id=5 --launch-time-ticks=50061387861 --mojo-platform-channel-handle=2612 --field-trial-handle=1696,i,12499043557648842706,1776622018909854588,131072 --disable-features=SpareRendererForSitePerProcess,WinRetrieveSuggestionsOnlyOnDemand /prefetch:1
  ******************************************************************************/
 
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
