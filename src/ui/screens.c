094243b87897e4e55c83dbe0d60ad69d6e97643b
Martin Vladic <martin.vladic@gmail.com>
2022-11-24 12:25:54 +0100

    update

--- src/ui/screens.c
+++ src/ui/screens.c
@@ -2,6 +2,7 @@
 #include "images.h"
 #include "fonts.h"
 #include "actions.h"
+#include "styles.h"
 #include "ui.h"
 
 objects_t objects;
@@ -797,6 +798,10 @@ void tick_screen_screen_3_setting() {
 
 
 void create_screens() {
+    lv_disp_t *dispp = lv_disp_get_default();
+    lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), false, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
+    
     create_screen_screen_1_print();
     create_screen_screen_2_move();
     create_screen_screen_3_setting();
